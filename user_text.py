32250d1b2fe17130ea6c13fe90f36a17aab5c845
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-10-11 13:00:14 -0600

    tinkering with requirements

--- user_text.py
+++ user_text.py
@@ -1,5 +1,14 @@
 # Goal is to allow the user to save strings, and then be able to send them later on
 
+# what is necessary:
+# if not peer-to-peer
+# the program must peroidically check for information being sent, and then store the mesasge
+# application must be always running (if not using other computers)
+
+# if peer-to-peer:
+# program must allow other computers to store encrypted data locally on the device
+# program must get the computer to send data when requested
+
 x = ""
 
 
