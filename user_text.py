2d8baa0baf7812bd89b5143aefa540c55d8d5b4b
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-10-13 17:27:39 -0600

    overriding the handle method in BaseRequestHandler

--- user_text.py
+++ user_text.py
@@ -1,21 +1,32 @@
 # Goal is to allow the user to save strings, and then be able to send them later on
 
 # what is necessary:
-# if not peer-to-peer
-# the program must peroidically check for information being sent, and then store the mesasge
-# application must be always running (if not using other computers)
+    # if not peer-to-peer
+    # the program must peroidically check for information being sent, and then store the mesasge
+    # application must be always running (if not using other computers)
 
-# if peer-to-peer:
-# program must allow other computers to store encrypted data locally on the device
-# program must get the computer to send data when requested
+    # if peer-to-peer:
+    # program must allow other computers to store encrypted data locally on the device
+    # program must get the computer to send data when requested
+
+    # program must force users to create a unique usnername, or else assign it to the user
+
 import socketserver
 
+
 class SocketServer:
-    # makes the device continuously serve requests
-    socketserver.ServerForever(1)
+    # code below makes the device continuously serve requests (but I don't know if I need it)
+    # socketserver.ServerForever(1)
+    def handle():
+        ''
 
 
-
+class BaseRequestHandler(SocketServer):
+    # for some reason I need this.
+    ''
+    # and for some reason I need to override this method here
+    def handle():
+        ''
 
 # user needs to do some specific action that lets them no longer take requests
 userAction = bool
